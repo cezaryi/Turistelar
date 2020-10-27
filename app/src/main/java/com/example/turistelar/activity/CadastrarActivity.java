2d550c5924f6047fb68ae6d07dea5dd50ed04a79
viniciusmmarques <51344091+viniciusmmarques@users.noreply.github.com>
2020-10-26 21:09:23 -0300

    tela cadastrar

---
+++ app/src/main/java/com/example/turistelar/activity/CadastrarActivity.java
@@ -0,0 +1,16 @@
+package com.example.turistelar.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.turistelar.R;
+
+public class CadastrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar);
+    }
+}
