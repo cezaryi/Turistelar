8a117baef1f9e99964643dccc80be506703260b8
cezaryi <cezariomourao@hotmail.com>
2020-11-12 22:22:05 -0300

    search fragment

--- app/src/main/java/com/example/turistelar/activity/SearchFragment.java
+++ app/src/main/java/com/example/turistelar/activity/SearchFragment.java
@@ -3,10 +3,12 @@ package com.example.turistelar.activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.turistelar.R;
 
@@ -55,6 +57,7 @@ public class SearchFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
